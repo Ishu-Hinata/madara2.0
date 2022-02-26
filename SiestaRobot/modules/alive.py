dec326f181143b41d9dd51b4155eeaab19d2601f
Lord dsp <87901742+Lord-DSP3@users.noreply.github.com>
2022-02-26 10:50:42 +0530

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -9,16 +9,16 @@ from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/ff2fa22dfa6ae838cc6cd.jpg"
+PHOTO = "https://telegra.ph/file/8b5265effe0dc1d43f829.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Siesta Robot.** \n\n"
-  TEXT += "💠 **I'm Working Properly** \n\n"
-  TEXT += f"💠 **My Master : [Vain](https://t.me/saint_foire)** \n\n"
-  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
-  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Siestaxbot?start=help"), Button.url("Support", "https://t.me/machinaxsupport")]]
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Ghost of Uchiha.** \n\n"
+  TEXT += " **𝐃𝐨 𝐧𝐨𝐭 𝐦𝐢𝐬𝐮𝐧𝐝𝐞𝐫𝐬𝐭𝐚𝐧𝐝 𝐭𝐡𝐢𝐬, 𝐭𝐡𝐢𝐬 𝐢𝐬 𝐧𝐨𝐭 𝐲𝐨𝐮𝐫 𝐩𝐨𝐰𝐞𝐫 𝐨𝐟 𝐜𝐫𝐞𝐚𝐭𝐢𝐨𝐧** \n\n"
+  TEXT += f"🎴 **My Master : [Your DAD](https://t.me/Lord_DSP_3)** \n\n"
+  TEXT += f"🍃 **Library Version :** `{telever}` \n\n"
+  TEXT += f"🎴 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"🍂 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "** Music alliance @UchihaClan_MusicBOT **"
+  BUTTON = [[Button.url("Help", "https://t.me/Uchiha_Madara_Robot?start=help"), Button.url("Support", "https://t.me/AnimeGroup_Chats")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
